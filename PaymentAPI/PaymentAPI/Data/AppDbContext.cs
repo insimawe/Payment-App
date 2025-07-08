a9a5a1cb178a908b165debbb809720c4d1fa7b0a
insimawe <canalita.lynsiemarie@gmail.com>
2025-07-08 23:07:55 +0800

    initial commit

---
+++ PaymentAPI/PaymentAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; } 
+    }
+}
